223da8d2bfabcf829d81afe09ad8d0bccf405c04
Max Power <alex@alexaa.dev>
2022-11-06 19:10:09 -0300

    feat(player): diagonal movement

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use crate::map::{xy_idx, TileType};
+use crate::map::{xy_to_idx, TileType};
 use crate::{BTerm, DenseVecStorage, Position, State, VirtualKeyCode, World};
 use specs::Component;
 use specs::{Join, WorldExt};
@@ -14,7 +14,7 @@ fn try_move_player(delta_x: i32, delta_y: i32, world: &mut World) {
     let map = world.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        let destination_idx = xy_to_idx(pos.x + delta_x, pos.y + delta_y);
 
         if map[destination_idx] == TileType::Floor {
             pos.x = min(79, max(0, pos.x + delta_x));
@@ -31,6 +31,10 @@ pub fn player_input(state: &mut State, ctx: &mut BTerm) {
             VirtualKeyCode::L | VirtualKeyCode::Right => try_move_player(1, 0, &mut state.world),
             VirtualKeyCode::K | VirtualKeyCode::Up => try_move_player(0, -1, &mut state.world),
             VirtualKeyCode::J | VirtualKeyCode::Down => try_move_player(0, 1, &mut state.world),
+            VirtualKeyCode::Y => try_move_player(-1, -1, &mut state.world),
+            VirtualKeyCode::U => try_move_player(1, -1, &mut state.world),
+            VirtualKeyCode::B => try_move_player(-1, 1, &mut state.world),
+            VirtualKeyCode::N => try_move_player(1, 1, &mut state.world),
             VirtualKeyCode::Q => ctx.quit(),
             _ => {}
         },
